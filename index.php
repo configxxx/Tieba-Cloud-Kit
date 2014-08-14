4925c926e07a0f0b10daf28a2e3234a9207d5bbc
racaljk <cthulhujk@gmail.com>
2014-08-14 15:00:03 +0800

    update

--- index.php
+++ index.php
@@ -27,14 +27,10 @@ $_list=tieba_list();
         <meta charset="UTF-8">
         <title>Tieba CLoud Kit</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-        <!-- bootstrap 3.0.2 -->
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <!-- font Awesome -->
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-        <!-- Ionicons -->
         <link href="css/ionicons.min.css" rel="stylesheet" type="text/css" />
-        <!-- Theme style -->
-        <link href="css/AdminLTE.css" rel="stylesheet" type="text/css" />
+        <link href="css/tck.css" rel="stylesheet" type="text/css" />
     </head>
 <!--#############################################################################################-->
     <body class="skin-blue">
